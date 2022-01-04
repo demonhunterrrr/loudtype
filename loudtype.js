5b1d5936467197fbbdb0df4cb593495b3d1f2528
demonhunterrrr <92695521+demonhunterrrr@users.noreply.github.com>
2022-01-04 17:30:46 -0500

    updated loudtype.js

--- loudtype.js
+++ loudtype.js
@@ -1,9 +1,20 @@
 function random(min, max) {
   return Math.round((max - min) * Math.random());
 }
-
+// Key detection template function
+function keyDecTemplate(kind) {
+  document.addEventListener(kind, function(e) {
+    if (e.code != 'Space' && e.code != 'Enter' && e.code != 'Backspace') {
+      const keysound = new Audio('GENERIC.mp3');
+      keysound.play();
+    }
+    else {
+      const keysound = new Audio(`${e.code}.mp3`);
+      keysound.play();
+    }
+  });
+}
 // Adding keypress event listener
-document.addEventListener("keypress", function(e) {
-  const keysound = new Audio(`${random(0,4)}.mp3`);
-  keysound.play();
-});
+keyDecTemplate('keypress');
+// Adding keyup event event listener
+keyDecTemplate('keyup');
