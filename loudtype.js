b44f1a27d936144724d80c3aa3f32eaadb7a4b8f
demonhunterrrr <92695521+demonhunterrrr@users.noreply.github.com>
2022-01-06 16:22:43 -0500

    (hopefully) did the final update to loudtype.js

--- loudtype.js
+++ loudtype.js
@@ -1,18 +1,30 @@
-function makeListeners(kind,key) {
-  const audio = new Audio(`http://demon--hunter.com/loudtype/holypanda/${kind}/${key}.mp3`);
-  document.addEventListener(kind, function(audio) {
-    audio.addEventListener('canplay', () =>{
-      audio.play()
-    })
+function random(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function makeListeners(kind) {
+  const generic = [];
+  const space = [];
+  const enter = [];
+  const backspace = [];
+
+  for (let x = 0;x < 3;x++) {
+    generic.push(new Audio(`http://demon--hunter.com/loudtype/cream/${kind}/GENERIC.mp3`));
+    space.push(new Audio(`http://demon--hunter.com/loudtype/cream/${kind}/Space.mp3`));
+    enter.push(new Audio(`http://demon--hunter.com/loudtype/cream/${kind}/Enter.mp3`));
+    backspace.push(new Audio(`http://demon--hunter.com/loudtype/cream/${kind}/Backspace.mp3`));
+  }
+  document.addEventListener(kind, function(e) {
+    if (e.code != 'Space' && e.code != 'Enter' && e.code !='Backspace') {
+      generic[random(0,2)].play();
+    }
+    else {
+      eval(e.code.toLowerCase())[random(0,2)].play();
+    }
   });
 }
 
-makeListeners('keypress','Space');
-makeListeners('keypress','Enter');
-makeListeners('keypress','Backspace');
-makeListeners('keypress','GENERIC');
-
-makeListeners('keyup','Space');
-makeListeners('keyup','Enter');
-makeListeners('keyup','Backspace');
-makeListeners('keyup','GENERIC');
+makeListeners('keypress');
+makeListeners('keyup');
