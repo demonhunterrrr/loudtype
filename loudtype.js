aae0cfa37993d08a50d186b13a9d58e836f911c1
demonhunterrrr <92695521+demonhunterrrr@users.noreply.github.com>
2022-01-06 13:19:52 -0500

    updated .js

--- loudtype.js
+++ loudtype.js
@@ -1,15 +1,16 @@
-function random(min, max) {
-  return Math.round((max - min) * Math.random());
-}
 function keyDecTemplate(kind) {
   document.addEventListener(kind, function(e) {
     if (e.code != 'Space' && e.code != 'Enter' && e.code != 'Backspace') {
-      const keysound = new Audio(`https://github.com/demonhunterrrr/loudtype/tree/main/holypanda/${kind}/GENERIC.mp3`);
-      keysound.play();
+      const keysound = new Audio(`http://demon--hunter.com/loudtype/holypanda/${kind}/GENERIC.mp3`);
+        keysound.addEventListener('canplay', () =>{
+          keysound.play();
+        });
     }
     else {
-      const keysound = new Audio(`https://github.com/demonhunterrrr/loudtype/tree/main/holypanda/${kind}/${e.code}.mp3`);
-      keysound.play();
+      const keysound = new Audio(`http://demon--hunter.com/loudtype/holypanda/${kind}/${e.code}.mp3`);
+      keysound.addEventListener('canplay', () =>{
+        keysound.play();
+      });
     }
   });
 }
